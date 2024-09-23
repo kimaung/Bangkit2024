1ff24974d80033b533fcc48b63af4f0ffe109dc8
aessaputra <aessaputra@yahoo.com>
2024-09-24 02:27:40 +0700

    Towards NodeJS Basics

---
+++ Belajar-Membuat-Aplikasi-Back-End-untuk-Pemula-dengan-Google-Cloud/nodejs-basic/process-object/index.js
@@ -0,0 +1,14 @@
+const initialMemoryUsage = 3463184;
+const yourName = "aessaputra";
+const environment = process.env.NODE_ENV || "development";
+
+for (let i = 0; i <= 10000; i++) {
+  // Proses looping ini akan membuat penggunaan memori naik
+}
+
+const currentMemoryUsage = process.memoryUsage().heapUsed;
+console.log(`Hai, ${yourName}`);
+console.log(`Mode environment: ${environment}`);
+console.log(
+  `Penggunaan memori dari ${initialMemoryUsage} naik ke ${currentMemoryUsage}`
+);
